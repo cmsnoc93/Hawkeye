f2dac5e263996b9018e63b5b8cd6c6445a40e3df
Neeraj Menon (neemenon) <neemenon@cisco.com>
2019-09-06 11:36:21 +0530

    InterfaceCounters fix

--- interfacecounters.py
+++ interfacecounters.py
@@ -17,15 +17,15 @@ def check_interfacecounters(ssh,nme,version,fhand,devicedict):
 						inter_dict[intfacek]=dict()
 						intfacek=intface
 					ret1 = ssh.send_command("sh int counters error | inc "+intface)
-				fhand.write("Show int counters error | ex 0\n")
-				fhand.write(ret1)
-				fhand.write("\n\n")
-				inter_dict[intface][j]=list()
-				if ret1:
-					ret1=ret1.split('\n')
-					for linex in ret1:
-						if linex.split()[0]==intface:
-							inter_dict[intface][j].append(linex.split()[1:])
+					fhand.write("Show int counters error | ex 0\n")
+					fhand.write(ret1)
+					fhand.write("\n\n")
+					inter_dict[intface][j]=list()
+					if ret1:
+						ret1=ret1.split('\n')
+						for linex in ret1:
+							if linex.split()[0]==intface:
+								inter_dict[intface][j].append(linex.split()[1:])
 						
 		devicedict.gennodedict['interface_counters_errors']=inter_dict
 		
